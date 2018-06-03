1bb4e644580fb8ced77659853aeab8c1bdde7c9c
Anshul <samgalacestyle01@gmail.com>
2018-06-03 16:25:42 +0530

    Adding Insertion Sort Code.

--- Sorting-InsertionSort.py
+++ Sorting-InsertionSort.py
@@ -1,5 +1,32 @@
 # Sorting - Insertion Sort
 
+
+def insertion_sort(a):
+    for i in range(1, len(a)):
+        j = i
+        while j > 0 and a[j - 1] > a[j]:
+            key = a[j]
+            a[j] = a[j - 1]
+            a[j - 1] = key
+            j -= 1
+    return a
+
+
 if __name__ == '__main__':
+
+    # Prompt the user to enter space separated integer array values
+    print('Enter space separated array elements to be sorted:')
+
     # Read the array elements from the input
-    arr = int(input.split())
+    arr = list(input().split())
+
+    # Convert the string input to integer values
+    arr = [int(i) for i in arr]
+
+    # Call the function for insertion sort
+    arr = insertion_sort(arr)
+
+    # Output:
+    print('Sorted Array:')
+    for i in arr:
+        print(i, end=' ')
