725ad0ff00e8efc0ea1ab4d48ddc0e77ba7bff50
Anshul <samgalacestyle01@gmail.com>
2018-06-03 19:14:13 +0530

    Adding Selection Sort Code

--- Sorting-SelectionSort.py
+++ Sorting-SelectionSort.py
@@ -1,14 +1,15 @@
-# Sorting - Insertion Sort
-# Insertion sort is like inserting one card at a time in a hand of cards
+# Sorting - Selection Sort
+# Iteratively find the minimum value in one sub-array and keep inserting it to the sorted sub-array
 
-def insertion_sort(a):
-    for i in range(1, len(a)):
-        j = i
-        while j > 0 and a[j - 1] > a[j]:
-            key = a[j]
-            a[j] = a[j - 1]
-            a[j - 1] = key
-            j -= 1
+
+def selection_sort(a):
+    for i in range(len(a)):
+        for j in range((i + 1), len(a)):
+            if a[i] > a[j]:
+                key = a[i]
+                a[i] = a[i + 1]
+                a[i + 1] = key
+
     return a
 
 
